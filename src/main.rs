c6b723bcf497b3baaf93ea609b4f87cda6e0ac74
Brian L. Troutwine <brian@troutwine.us>
2017-12-05 23:21:08 -0800

    Introduce 'Actions', repeated experiments
    This commit introduces 'Actions', a record of what each actor in the
    system has done. The notion here is that we'll use the two actor queues
    -- called SENDER_Q and RECEIVER_Q -- to debug experiment runs that go
    sideways. This commit also incorporates results from the Action queues
    which mean we now are much more likely to run to a successful completion.
    As a result, the old main loop is more or less split out into an
    'experiment' run and the main loop just runs this over and over until we
    hit catastrophic failure.
    
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate byteorder;
+extern crate coco;
 #[macro_use]
 extern crate lazy_static;
 extern crate memmap;
@@ -14,9 +15,34 @@ use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 lazy_static! {
     static ref TOTAL_SENDERS: sync::Arc<AtomicUsize> = sync::Arc::new(AtomicUsize::new(0));
     static ref VALUES_TO_READ: sync::Arc<AtomicUsize> = sync::Arc::new(AtomicUsize::new(0));
+    static ref SENDER_Q: coco::Stack<Action> = coco::Stack::new();
+    static ref RECEIVER_Q: coco::Stack<Action> = coco::Stack::new();
 }
 
+static CAP: usize = 10_000;
 static PATH: &'static str = "/tmp/mmap_comm";
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum Action {
+    Write { offset: usize, val: u64 },
+    Read { offset: usize, val: u64 },
+}
+
+impl Action {
+    fn is_write(&self) -> bool {
+        match *self {
+            Action::Write { .. } => true,
+            _ => false,
+        }
+    }
+
+    fn is_read(&self) -> bool {
+        match *self {
+            Action::Read { .. } => true,
+            _ => false,
+        }
+    }
+}
 
 fn receiver() -> u64 {
     let path: PathBuf = PathBuf::from(PATH);
@@ -26,8 +52,7 @@ fn receiver() -> u64 {
         .create(true)
         .open(&path)
         .unwrap();
-    file.set_len((mem::size_of::<u64>() * 10_000) as u64)
-        .unwrap();
+    file.set_len((mem::size_of::<u64>() * CAP) as u64).unwrap();
     let mmap = unsafe { memmap::MmapMut::map_mut(&file).unwrap() };
 
     let mut summation: u64 = 0;
@@ -37,11 +62,16 @@ fn receiver() -> u64 {
 
     loop {
         while VALUES_TO_READ.load(Ordering::SeqCst) != 0 {
+            buf.seek(SeekFrom::Start(0)).unwrap();
             buf.write_all(&mmap[offset..offset + 8]).unwrap();
             buf.seek(SeekFrom::Start(0)).unwrap();
             offset += 8;
 
             let val = buf.read_u64::<BigEndian>().unwrap();
+            RECEIVER_Q.push(Action::Read {
+                offset: offset - 8,
+                val: val,
+            });
             summation += val;
 
             VALUES_TO_READ.fetch_sub(1, Ordering::SeqCst);
@@ -63,8 +93,7 @@ fn sender() -> u64 {
         .create(true)
         .open(&path)
         .unwrap();
-    file.set_len((mem::size_of::<u64>() * 10_000) as u64)
-        .unwrap();
+    file.set_len((mem::size_of::<u64>() * CAP) as u64).unwrap();
     let mut mmap = unsafe { memmap::MmapMut::map_mut(&file).unwrap() };
 
     TOTAL_SENDERS.fetch_add(1, Ordering::SeqCst);
@@ -75,16 +104,20 @@ fn sender() -> u64 {
     let mut idx = 0;
     let mut summation: u64 = 0;
     let mut buf = vec![]; // TODO can we write directly to mmap?
-    while idx < 10_000 {
-        let val = rng.gen::<u8>() as u64; // don't want to overflow...
+    while idx < CAP {
+        let val = u64::from(rng.gen::<u8>()); // don't want to overflow...
         summation += val;
         buf.write_u64::<BigEndian>(val)
             .expect("SERIALIZING THE VAL");
-        for i in 0..8 {
-            mmap[offset] = buf[i];
+        SENDER_Q.push(Action::Write {
+            offset: offset,
+            val: val,
+        });
+        for byte in buf.iter().take(8) {
+            mmap[offset] = *byte;
             offset += 1;
         }
-        // nothing intentionally
+        buf.clear();
         VALUES_TO_READ.fetch_add(1, Ordering::SeqCst);
         idx += 1;
     }
@@ -94,7 +127,7 @@ fn sender() -> u64 {
     summation
 }
 
-fn main() {
+fn experiment() {
     let _ = fs::remove_file(PATH);
 
     let sender_join = thread::spawn(sender);
@@ -103,5 +136,56 @@ fn main() {
     let sender_sum = sender_join.join().unwrap();
     let receiver_sum = receiver_join.join().unwrap();
 
+    let mut sender_actions: Vec<Action> = Vec::new();
+    while let Some(action) = SENDER_Q.pop() {
+        assert!(action.is_write());
+        if let Err(idx) = sender_actions.binary_search(&action) {
+            sender_actions.insert(idx, action)
+        } else {
+            unreachable!()
+        }
+    }
+    let mut receiver_actions: Vec<Action> = Vec::new();
+    while let Some(action) = RECEIVER_Q.pop() {
+        assert!(action.is_read());
+        if let Err(idx) = receiver_actions.binary_search(&action) {
+            receiver_actions.insert(idx, action)
+        } else {
+            unreachable!()
+        }
+    }
+
+    assert_eq!(sender_actions.len(), receiver_actions.len());
+    for tup in sender_actions.iter().zip(receiver_actions.iter()) {
+        match tup {
+            (
+                &Action::Write {
+                    offset: lo,
+                    val: lv,
+                },
+                &Action::Read {
+                    offset: ro,
+                    val: rv,
+                },
+            ) => {
+                if (lo != ro) || (lv != rv) {
+                    panic!("DIFFERED ON OPERATION PAIR: {:?}", tup);
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+
     assert_eq!(sender_sum, receiver_sum);
+}
+
+fn main() {
+    // It's possible that someone will run this on a u32 system. Until AtomicU64
+    // becomes stable we are limited to running this friendly program on 64 bit
+    // systems.
+    assert_eq!(mem::size_of::<usize>(), mem::size_of::<u64>());
+
+    loop {
+        experiment()
+    }
 }
